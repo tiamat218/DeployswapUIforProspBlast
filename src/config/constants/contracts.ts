e1936b4c8a3c0ab22481486702a481f9e83ec6d8
tiamat218 <cebe.devrim@gmail.com>
2024-07-15 13:34:16 +0200

    Deploy

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,70 @@
+export default {
+  masterChef: {
+    97: '',
+    56: '',
+  },
+  sousChef: {
+    97: '',
+    56: '',
+  },
+  lotteryV2: {
+    97: '',
+    56: '',
+  },
+  multiCall: {
+    56: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
+    97: '',
+  },
+  pancakeProfile: {
+    56: '',
+    97: '',
+  },
+  pancakeRabbits: {
+    56: '',
+    97: '',
+  },
+  bunnyFactory: {
+    56: '',
+    97: '',
+  },
+  claimRefund: {
+    56: '',
+    97: '',
+  },
+  pointCenterIfo: {
+    56: '',
+    97: '',
+  },
+  bunnySpecial: {
+    56: '',
+    97: '',
+  },
+  tradingCompetition: {
+    56: '',
+    97: '',
+  },
+  easterNft: {
+    56: '',
+    97: '',
+  },
+  cakeVault: {
+    56: '',
+    97: '',
+  },
+  predictions: {
+    56: '',
+    97: '',
+  },
+  chainlinkOracle: {
+    56: '',
+    97: '',
+  },
+  bunnySpecialCakeVault: {
+    56: '',
+    97: '',
+  },
+  bunnySpecialPrediction: {
+    56: '',
+    97: '',
+  },
+}
